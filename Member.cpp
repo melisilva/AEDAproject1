2e5a27d0230bd9ff19392e4da69c10ddb18b8a73
mathfsilva <mathfsilva14@gmail.com>
2020-10-12 22:43:00 +0100

    Book e Member mais desenvolvidos

--- Member.cpp
+++ Member.cpp
@@ -1,9 +1,12 @@
+#include <fstream>
 #include "Member.h"
+using namespace std;
 
-Member::Member(string name, int nif, vector<Book*> books) {
+Member::Member(string &name, int &nif, vector<Book*> &books) {
     this->name = name;
     this->nif = nif;
     this->books = books;
+    this->balance = 50;
 }
 
 int Member::getNIF() const {
@@ -33,14 +36,19 @@ int Member::findBook(string title) const{
 }
 
 bool Member::makeRequest(int code, string name){
+    string date;
+
+    cout << "Indique a data no formato DD-MM-YYYY: ";
+    cin >> date;
+
     if ((code == 0) && (name == "")) {
         return false;
     }
     if (code != 0){
-        lendRequests.push_back(*books[code]);
+        lendRequests.push_back(make_pair(books[code], date));
         return true;
     } else if ((name != "") && (findBook(name) != -1)) {
-        lendRequests.push_back(*books[findBook(name)]);
+        lendRequests.push_back(make_pair(books[findBook(name)], date));
         return true;
     }
     return false;
@@ -52,9 +60,84 @@ bool Member::showLendRequests() const {
     }
 
     for (int i = 0; i < lendRequests.size(); i++){
-        cout << (*lendRequests[i]).getTitle() << " by " << (*lendRequests[i]).getAuthor();
+        cout << (*lendRequests[i].first).getTitle() << " por " << (*lendRequests[i].first).getAuthor();
     }
 
     return true;
+}
+
+void Member::showData() const {
+    stringstream temp;
+
+    temp << "Nome: " << name << endl << "NIF: " << nif << endl << "Livros: " << endl;
+    for (int i = 0; i < books.size(); i++){
+        temp << "   -" << (*books[i]).getTitle() << " por " << (*books[i]).getAuthor() << endl;
+    }
+
+    temp << "Pedidos: " << endl;
+    for (int i = 0; i < lendings.size(); i++){
+        temp << "   -" << (*lendings[i].first).getTitle() << " por " << (*lendings[i].first).getAuthor() << endl;
+    }
+
+    temp << "Empréstimos em Vigor: " << endl;
+    for (int i = 0; i < lendings.size(); i++){
+        temp << "   -" << (*lendings[i].first).getTitle() << " por " << (*lendings[i].first).getAuthor() << endl;
+    }
+
+    cout << temp.str();
+}
+
+void Member::showBooks() const {
+    cout << "Livros de " << getName() << ": " << endl;
+    for (int i = 0; i < books.size(); i++){
+        cout << "   -" << (*books[i]).getTitle() << " por " << (*books[i]).getAuthor() << endl;
+    }
+}
+
+void Member::showLendings() const {
+    cout << "Empréstimos em Vigor: " << endl;
+    for (int i = 0; i < lendings.size(); i++){
+        cout << "   " << i + 1 << " - " << (*lendings[i].first).getTitle() << " por " << (*lendings[i].first).getAuthor() << endl;
+    }
+}
+
+void Member::outputData() const {
+    ofstream file("Member.txt");
+
+    stringstream temp;
+
+    temp << "Nome: " << name << endl << "NIF: " << nif << endl << "Livros: " << endl;
+    for (int i = 0; i < books.size(); i++){
+        temp << "   -" << (*books[i]).getTitle() << " por " << (*books[i]).getAuthor() << endl;
+    }
+
+    temp << "Pedidos: " << endl;
+    for (int i = 0; i < lendings.size(); i++){
+        temp << "   -" << (*lendings[i].first).getTitle() << " por " << (*lendings[i].first).getAuthor() << endl;
+    }
+
+    temp << "Empréstimos em Vigor: " << endl;
+    for (int i = 0; i < lendings.size(); i++){
+        temp << "   -" << (*lendings[i].first).getTitle() << " por " << (*lendings[i].first).getAuthor() << endl;
+    }
+
+    file << temp.str();
+}
+
+void Member::renovateLending() const {
+    int temp;
+    string date;
+
+    //Informamos o utilizador dos empréstimos em vigor.
+    showLendings();
+
+    cout << "Por favor, indique a ordem do empréstimo: ";
+    cin >> temp;
+    cout << "Por favor, indique a data de hoje (formato DD-MM-YY): ";
+    cin >> date;
+
+    //Preciso testar que a data está nos últimos 3 dias do empréstimo em vigor!
+    replace(lendings.begin(), lendings.end(), lendings[temp - 1], pair<Book*, string>(lendings[temp - 1].first, date));
+
 }
 
--- club.h
+++ club.h
@@ -1,26 +1,3 @@
-#ifndef AEDAPROJECT1_CLUB_H
-#define AEDAPROJECT1_CLUB_H
-#include "Book.h"
-#include "Catalog.h"
 #include "Member.h"
-#include "nonMem.h"
-using namespace std;
+#include "catalog.h"
 
-class Club {
-    Catalog catalog;
-    vector<Member> members;
-
-public:
-
-    void addMember(); //pede o nome e NIF e depois os livros;
-    void removeMember();
-    void chargeDelay(int nif, int account);
-    void chargeFee(int nif, int account);
-    void showMembers();
-    void showLendings();
-    void showDelays();
-    void showLendRequests();
-};
-
-
-#endif //AEDAPROJECT1_CLUB_H
